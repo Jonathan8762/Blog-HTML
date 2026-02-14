902b3d3fd9ccbec1232ef8ded51e9b65c70e3791
Jonathan Enmanuel Pauth <168257556+Jonathan8762@users.noreply.github.com>
2026-02-14 12:51:29 -0600

    correcciones

---
+++ JS/menulaterarl.js
@@ -0,0 +1,10 @@
+
+function menulateral() {
+  const sidebar = document.getElementById("mySidebar");
+  
+  if (sidebar.style.width === "250px") {
+    sidebar.style.width = "0";
+  } else {
+    sidebar.style.width = "250px";
+  }
+}
